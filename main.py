b19d63f3f386ebdc19ef6729ef053e795f640401
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-08-23 21:31:21 +0530

    Add File Organizer script.

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import shutil 
+
+path = input("Enter the path of the folder you want to organize: ")
+files = os.listdir(path)
+
+for file in files:
+    filename, extentions = os.path.splitext(file)
+    extentions = extentions[1:]
+    
+    if os.path.exists(path+"/"+extentions):
+        shutil.move(path+"/"+file, path+"/"+extentions+"/"+file)
+    else:
+        os.makedirs(path+"/"+extentions)
+        shutil.move(path+"/"+file, path+"/"+extentions+"/"+file)
